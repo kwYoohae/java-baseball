21d740a7edf14ae1b8223b66b2978ee44ea0c4ef
leech1211 <leech1211@nate.com>
2022-12-28 18:57:59 +0900

    feat(Manager): 생성자를 통한 랜덤숫자 생성메서드 생성

--- src/main/java/baseball/Manager.java
+++ src/main/java/baseball/Manager.java
@@ -1,2 +1,30 @@
-package baseball;public class Manager {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Manager {
+
+    private int[] number;     //number array
+
+
+    Manager()       //constructor
+    {
+        makeNumber();       //create number
+    }
+
+    private void makeNumber()       //create number
+    {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        System.out.println(computer);
+    }
 }
