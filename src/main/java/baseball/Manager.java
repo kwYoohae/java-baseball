9f0f79db53bc4e61d6ac903c8001bf596f0379e9
leech1211 <leech1211@nate.com>
2022-12-28 19:32:17 +0900

    style(Manager): 랜덤숫자 생성자 메서드 makeNumber로 분리

--- src/main/java/baseball/Manager.java
+++ src/main/java/baseball/Manager.java
@@ -3,16 +3,18 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Manager {
 
-    private int[] number;     //number array
+    private int []number;     //number array
 
 
     Manager()       //constructor
     {
-        makeNumber();       //create number
+        System.out.println("숫자 야구 게임을 시작합니다.");     //opening message
+        number = new int[3];        //create number array
     }
 
     private void makeNumber()       //create number
@@ -24,7 +26,16 @@ public class Manager {
                 computer.add(randomNumber);
             }
         }
-
-        System.out.println(computer);
+        setNumber(computer);        //setNumber
     }
+    private void setNumber(List<Integer> computer)      //setNumber
+    {
+        for (int i=0;i<3;i++)
+        {
+            int n = Integer.parseInt(computer.get(i).toString());   // Integer to int
+            this.number[i] = n;     //set number
+        }
+    }
+
+
 }
