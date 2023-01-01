75aed5eb38ef69b50fd5a4152eea944b64932515
bbangjun <lucid9dawn@gmail.com>
2023-01-02 02:38:14 +0900

    feat(Computer): 스트라이크 갯수 판단 메서드 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -12,16 +12,17 @@ public class Computer {
     Exception exception = new Exception();
     List<Integer> userNumList = new ArrayList<>(); // ArrayList for user's 3 numbers
     List<Integer> randomNum = new ArrayList<>(); // ArrayList for make 3 random numbers
+    Integer numOfBall = 0, numOfStrike = 0;
 
     public void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         makeRandomNum();
         inputNum();
         exception.userNumberException(userNumList);
+        judgeAnswer();
     }
 
     public void makeRandomNum(){ // make 3 random number
-
         while (randomNum.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!randomNum.contains(randomNumber)) {
@@ -33,5 +34,32 @@ public class Computer {
     public void inputNum(){
         System.out.println("숫자를 입력해주세요 : ");
         userNumList=user.getUserNumList();
+    }
+
+    public void judgeBall(Integer i){
+        if(randomNum.get(i) != userNumList.get(i)) {
+
+        }
+    }
+
+    public void judgeStrike(Integer i){
+        if(randomNum.get(i) == userNumList.get(i)) {
+            numOfStrike++;
+        }
+    }
+
+    public void judgeNothing(Integer i){
+
+    }
+
+    public void judgeAnswer(){
+        while(userNumList == randomNum){
+            System.out.println("숫자를 입력해주세요 : ");
+            for(int i = 0; i < 3; i++){
+                judgeStrike(i);
+                judgeBall(i);
+                judgeNothing(i);
+            }
+        }
     }
 }
