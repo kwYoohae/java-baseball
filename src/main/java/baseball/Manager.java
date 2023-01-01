365a166d8ff164bb02f5c85c4d7b25af8e1756f2
방지민 <bjimin7@naver.com>
2023-01-01 20:02:04 +0900

    feat(Manager): 입력한 수와 랜덤수를 비교한 strike, ball 결과 계산

--- src/main/java/baseball/Manager.java
+++ src/main/java/baseball/Manager.java
@@ -1,2 +1,39 @@
-package baseball;public class Manager {
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Manager {
+
+    ArrayList<Integer> computerNumbers;
+    ArrayList<Integer> userNumbers;
+    void run(){ //game run
+        try {
+            Number numberClass = new Number(1, 9);
+            this.computerNumbers = numberClass.getComputerNumbers();
+            while(true) {
+                this.userNumbers = numberClass.getUserNumbers();
+                int result = comparisonNumbers();
+                userNumbers.clear();
+
+                if(result == 30)
+                    break;
+            }
+        } catch (Exception e) {
+            //게임 종료, 게임종료시 ArrayList 초기화 필요
+        }
+    }
+
+    int comparisonNumbers(){
+        int strike = 0;
+        int ball = 0;
+        for(int i=0; i<3; i++){
+            if(Objects.equals(computerNumbers.get(i), userNumbers.get(i)))
+                strike++;
+            else if(computerNumbers.contains(userNumbers.get(i)))
+                ball++;
+        }
+
+        return strike*10 + ball;
+    }
 }
