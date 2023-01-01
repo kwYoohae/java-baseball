07149516e19edd54d85396447e412971fde5c8a5
방지민 <bjimin7@naver.com>
2023-01-02 00:22:10 +0900

    feat(Application): 게임 시작 및 재시작, 종료 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int game = 0;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Manager m = new Manager();
+        do{
+            m.run();
+            String state = readLine();
+            if(state.length()!=1)
+                throw new IllegalArgumentException("잘못된 값 입력");
+            if(state.charAt(0) != '1' && state.charAt(0) != '2')
+                throw new IllegalArgumentException("잘못된 값 입력");
+            game = (state.charAt(0) - '0');
+        } while(game == 1);
+        System.out.println("게임 종료");
     }
 }
