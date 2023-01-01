26f18f22508db1f9b78942750687deb27af561cf
방지민 <bjimin7@naver.com>
2023-01-02 00:35:12 +0900

    chore(Number): 숫자 입력 요청 출력 로직 추가

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -31,11 +31,11 @@ public class Number {
 
     ArrayList<Integer> getUserNumbers(){ //사용자에게 1~9 사이의 서로 다른 수 입력받는 메서드
         ArrayList<Integer> userNumbers = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
         String numbers = readLine(); //get string without separator
 
         if(numbers.length() != 3) //적게 또는 많이 입력했을 때
             throw new IllegalArgumentException("입력받은 문자열이 3보다 길거나 짧음");
-
         for(int i=0; i<3; i++){
             char c = numbers.charAt(i);
             if('0' > c || '9' < c) //숫자가 아닐 때
