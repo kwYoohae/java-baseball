4d946c501d5bca595700885e89575090e0223602
leech1211 <leech1211@nate.com>
2023-01-16 16:32:52 +0900

    fix(Application): 클래스쪼개기에 따른 Application 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        Manager playBall = new Manager();
-        playBall.gameStart();
+       // Manager playBall = new Manager();
+        //playBall.gameStart();
+
+        numberBaseball play = new numberBaseball();
+        play.playBall();
     }
 }
