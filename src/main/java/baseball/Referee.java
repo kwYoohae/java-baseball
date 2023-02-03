78af0f9d25930edbe5148fb3982d72be6ee13042
leech1211 <leech1211@nate.com>
2023-02-03 15:41:02 +0900

    fix(Referee) : 3 상수사용

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -7,21 +7,23 @@ public class Referee {
     private int strike;
     private int ball;
     private List<Integer> userCount;        //유저의 입력 카운트
+    private int BIT;
 
-    public Referee()
+    public Referee(int b)
     {
         strike = 0;
         ball = 0;
         userCount = new ArrayList<>();
+        BIT = b;
     }
 
     private void setUserCount(int num)
     {
-        int []userNumber = new int[3];
+        int []userNumber = new int[BIT];
         userNumber[0] = num / 100;
         userNumber[1] = num % 100 / 10;
         userNumber[2] = num % 10;
-        for(int i=0;i<3;i++)
+        for(int i=0;i<BIT;i++)
         {
             userCount.add(userNumber[i]);
         }
@@ -30,7 +32,7 @@ public class Referee {
     private void setStrike(List<Integer> ballCount)     //is it strike?
     {
         int result = 0;
-        for(int i=0;i<3;i++)        //숫자와 자리가 같은경우
+        for(int i=0;i<BIT;i++)        //숫자와 자리가 같은경우
         {
             if(userCount.get(i) == ballCount.get(i))
             {
@@ -43,7 +45,7 @@ public class Referee {
     private void setBall(List<Integer> ballCount)     //is it ball?
     {
         int result = 0;
-        for(int i=0;i<3;i++)
+        for(int i=0;i<BIT;i++)
         {
             if(userCount.get(i) != ballCount.get(i) && inCount(userCount.get(i),ballCount))        //숫자는 같으나 자리는 다른경우
             {
@@ -55,7 +57,7 @@ public class Referee {
 
     private boolean inCount(int n,List<Integer> ballCount)       //is that index in number?
     {
-        for(int i=0;i<3;i++)        //다른자리에 값이 있는경우
+        for(int i=0;i<BIT;i++)        //다른자리에 값이 있는경우
         {
             if(ballCount.get(i) == n)
             {
@@ -84,7 +86,7 @@ public class Referee {
 
     public void empty()
     {
-        for (int i=2;i>=0;i--)
+        for (int i=BIT-1;i>=0;i--)
         {
             userCount.remove(i);
         }
