1fb2232654e1dd0b2e58f634f2498f0fffa0c7a5
leech1211 <leech1211@nate.com>
2023-02-03 15:24:03 +0900

    fix(Application) : 의미없는 공백

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-       // Manager playBall = new Manager();
-        //playBall.gameStart();
-
         numberBaseball play = new numberBaseball();
         play.playBall();
     }
